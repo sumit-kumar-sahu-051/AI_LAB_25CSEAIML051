cfd9939fee32f41a872189f006b736e8e66a8503
sumit <25cseaiml051.sumitkumarsahu@giet.edu>
2026-07-25 17:52:00 +0530

    Added BFS Graph Traversal with Target Search Program

--- BFS.py
+++ BFS.py
@@ -1,4 +1,4 @@
-def bfs(graph , start_code) :
+def bfs(graph, start_code, target_node) :
     visited = []
     queue = [start_code]
 
@@ -8,44 +8,62 @@ def bfs(graph , start_code) :
             print(f"Exploring node : {current_node}")
             visited.append(current_node)
 
-            for neighbour in graph.get(current_node , []) :
+            # Check if target node is found
+
+            if current_node == target_node :
+                print(f"\nTarget node '{target_node}' found!")
+                return visited
+
+            for neighbour in graph.get(current_node, []) :
                 if neighbour not in visited and neighbour not in queue :
                     queue.append(neighbour)
 
+    print(f"\nTarget node '{target_node}' not found!")
     return visited
+
 
 #-----------User input section----------------
 
-print("----Build your graph-----")
+print("-------------BFS Graph Traversal with Target Search----------")
+print("\n----Build your graph-----")
+
 student_graph = {}
 
 #----------Get the total number of connections------------------
 
-num_edges = int(input("How many edges (connections) does your graph have : "))
-print("Enter each edge separated by a space (e.g., A B) : ")
+num_edges = int(input("\nHow many edges (connections) does your graph have : "))
+print("\nEnter each edge separated by a space (e.g., A B) : ")
 
 for i in range(num_edges):
 
     # Read the edge and split it into two variables
 
-    u , v = input(f"Edge {i+1} : ").split()
+    u, v = input(f"Edge {i+1} : ").split()
 
-    # initialize the lists if the nodes doesn't exist yet
+    # Initialize the lists if the nodes don't exist yet
 
-    if u not in student_graph:
+    if u not in student_graph :
         student_graph[u] = []
-    if v not in student_graph:
+
+    if v not in student_graph :
         student_graph[v] = []
 
-    # Add the connection (Unirected graph)
+    # Add the connection (Undirected graph)
 
     student_graph[u].append(v)
     student_graph[v].append(u)
 
-#Get the starting point
+# Get the starting point
 
-start = input("Enter the starting node for BFS : ")
+start = input("\nEnter the starting node for BFS : ")
+
+# Get the target node
+
+target = input("Enter the target node : ")
 
 print(f"\nYour graph dictionary : {student_graph}")
-print("Starting BFS traversal.......")
+print("\nStarting BFS traversal.......\n")
 
+visited_nodes = bfs(student_graph, start, target)
+
+print("\nVisited Nodes :", visited_nodes)
